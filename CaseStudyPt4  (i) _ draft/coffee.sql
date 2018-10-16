53065b10b2611518d70d8f0617e505204d08c74e
fnj1017 <fnj1017@gmail.com>
2018-10-16 12:07:40 +0800

    test_success status_not actually updated

--- CaseStudyPt4  (i) _ draft/coffee.sql
+++ CaseStudyPt4  (i) _ draft/coffee.sql
@@ -1,20 +1,39 @@
+/*
 create table just
   (
     size char(50) not null,
     price float(2) not null,
-    description char(200) not null
+    --description char(200) not null
   );
 
 create table cafe
   (
     size char(50) not null,
     price float(2) not null,
-    description char(200) not null
+    --description char(200) not null
   );
 
   create table cappuccino
     (
       size char(50) not null,
       price float(2) not null,
-      description char(200) not null
+      --description char(200) not null
     );
+*/
+
+    CREATE TABLE `menu` (
+      `name` char(50) NOT NULL,
+      `size` char(50) NOT NULL,
+      `price` float NOT NULL
+    );
+
+    INSERT INTO `menu` (`name`, `size`, `price`) VALUES
+    ('Just Java', 'Endless Cup', 2.00),
+    ('Cafe au Lait', 'Single', 2.00),
+    ('Cafe au Lait', 'Double', 3.00),
+    ('Iced Cappuccino', 'Single', 4.75),
+    ('Iced Cappuccino', 'Double', 5.75);
+
+
+--  ALTER TABLE just
+--    MODIFY size char(50) not null;
--- CaseStudyPt4  (i) _ draft/update_coffee.php
+++ CaseStudyPt4  (i) _ draft/update_coffee.php
@@ -1,16 +1,10 @@
-<html>
-<head>
-  <title>JavaJam Coffee House</title>
-</head>
-<body>
-  <h1> JavaJam Coffee Entry Result</h1>
 <?php
   //Create short variable names
 
   $selectedName = $_POST['selectedName'];
   $selectedSize = $_POST['selectedSize'];
   $selectedPrice = $_POST['selectedPrice'];
-  $selectedDescription = $_POST['selectedDecription'];
+  //$selectedDescription = $_POST['selectedDecription'];
 
   if (!$selectedName || !$selectedSize || !$selectedPrice) {
      echo "You have not entered all the required details.<br />"
@@ -18,6 +12,7 @@
      exit;
   }
 
+/*
   if(!$selectedDescription){
     if($selectedName == just){
       $selectedDescription = "Regular house blend, decaffeinated coffee, or flavor of the day.";
@@ -27,33 +22,34 @@
       $selectedDescription = "Sweetened espresso blended witj icy-cold milk and served in a chilled glass.";
     }
   }
+*/
 
   if (!get_magic_quotes_gpc()) {
     $selectedName = addslashes($selectedName);
     $selectedSize = addslashes($selectedSize);
     $selectedPrice = doubleval($selectedPrice);
-    $selectedDescription = addslashes($selectedDescription);
+    //$selectedDescription = addslashes($selectedDescription);
   }
 
-  @ $db = new mysqli('localhost', 'f37ee', 'f37ee', 'f37ee');
+  //@ $db = new mysqli('localhost', 'f37ee', 'f37ee', 'f37ee');
+@ $db = mysqli_connect("localhost", "f37ee", "f37ee", "f37ee");
 
   if (mysqli_connect_errno()) {
      echo "Error: Could not connect to database.  Please try again later.";
      exit;
   }
 
-  $query = "UPDATE $selectedName SET price = $selectedPrice and description = $selectedDescription WHERE size = $selectedSize";
+  $query = "UPDATE `menu` SET `price` = $selectedPrice WHERE `name`=  '".$selectedName."' AND `size` = '".$selectedSize."'   ";
   //$query = "insert into just values
     //        ('".$selectedSize."', '".$selectedPrice."', '".$selectedDescription."')";
 
   $result = $db->query($query);
 
   if($result){
-    echo  $db->affected_rows." coffee updated into database.";
+    echo " coffee updated into database.";
   }else{
     echo "An error has occurred.  The item was not updated.";
   }
   $db->close();
+  echo '<a href="update_coffee.html">Back</a>';
  ?>
-</body>
-</html>
