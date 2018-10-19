1d2f4a43008ecb8e8d77be3d45fb792f32ebf72d
fnj1017 <fnj1017@gmail.com>
2018-10-19 23:12:30 +0800

    inserted order info

--- CaseStudyPt4  (i) _ draft/coffee.sql
+++ CaseStudyPt4  (i) _ draft/coffee.sql
@@ -21,6 +21,38 @@ create table cafe
     );
 */
 
+    CREATE TABLE `order` (
+      `item` char(50) NOT NULL,
+      `size` char(50) NOT NULL,
+      `qty` int(10) NOT NULL,
+      `amount` float NOT NULL
+    );
+
+    INSERT INTO `order` (`item`, `size`, `qty`, `amount`) VALUES
+    ('Just Java', 'Endless Cup', 3, 6.00),
+    ('Just Java', 'Endless Cup', 1, 2.00),
+    ('Just Java', 'Endless Cup', 1, 2.00),
+    ('Just Java', 'Endless Cup', 5, 10.00),
+    ('Just Java', 'Endless Cup', 1, 2.00),
+    ('Just Java', 'Endless Cup', 4, 8.00),
+    ('Cafe au Lait', 'Single', 1, 2.00),
+    ('Cafe au Lait', 'Double', 1, 3.00),
+    ('Iced Cappuccino', 'Single', 1, 4.75),
+    ('Iced Cappuccino', 'Double', 1, 5.75),
+    ('Cafe au Lait', 'Single', 3, 6.00),
+    ('Cafe au Lait', 'Double', 1, 3.00),
+    ('Iced Cappuccino', 'Single', 1, 4.75),
+    ('Iced Cappuccino', 'Double', 1, 5.75),
+    ('Cafe au Lait', 'Single', 1, 2.00),
+    ('Cafe au Lait', 'Double', 2, 6.00),
+    ('Iced Cappuccino', 'Single', 1, 4.75),
+    ('Iced Cappuccino', 'Double', 1, 5.75),
+    ('Cafe au Lait', 'Single', 1, 2.00),
+    ('Cafe au Lait', 'Double', 2, 6.00),
+    ('Iced Cappuccino', 'Single', 1, 4.75),
+    ('Iced Cappuccino', 'Double', 1, 5.75);
+
+
     CREATE TABLE `menu` (
       `name` char(50) NOT NULL,
       `size` char(50) NOT NULL,
